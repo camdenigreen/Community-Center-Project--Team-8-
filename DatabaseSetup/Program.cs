549f87848593d2bb48a96259f792c446f7b72bba
Josh Zrubek <joshzru@gmail.com>
2024-04-01 21:06:43 -0500

    Program properly drops and creates all tables.

--- DatabaseSetup/Program.cs
+++ DatabaseSetup/Program.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+using System.Transactions;
 
 namespace DatabaseSetup
 {
@@ -10,7 +14,16 @@ namespace DatabaseSetup
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            const string SERVER= @"(localdb)\MSSQLLocalDb";
+            const string DATABASE = @"communitycenter";
+
+            // DRIVER={{ODBC Driver 17 for SQL Server}};SERVER=(localdb)\\MSSQLLocalDb;DATABASE=WideWorldImporters;Trusted_Connection=yes
+
+            string connectionString = string.Format(@"SERVER={0};DATABASE={1};INTEGRATED SECURITY=SSPI;Trusted_Connection=yes", SERVER, DATABASE);
+
+            Setup setupObject = new Setup(connectionString);
+
+            setupObject.RebuildTables();
         }
     }
 }
--- DatabaseSetup/Setup.cs
+++ DatabaseSetup/Setup.cs
@@ -1,13 +1,49 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Transactions;
+using System.IO;
 
 namespace DatabaseSetup
 {
-    public static class Setup
+    public class Setup
     {
+        private readonly string _connectionString;
 
+        public Setup(string connectionString)
+        {
+            this._connectionString = connectionString;
+        }
+
+        public void RebuildTables()
+        {
+            string setupScript = File.ReadAllText("../../Tables.sql");
+
+            using (TransactionScope transaction = new TransactionScope())
+            {
+                using (SqlConnection connection = new SqlConnection(_connectionString))
+                {
+                    using (SqlCommand command = new SqlCommand(setupScript, connection))
+                    {
+                        command.CommandType = CommandType.Text;
+
+                        connection.Open();
+
+                        command.ExecuteNonQuery();
+
+                        transaction.Complete();
+                    }
+                }
+            }
+        }
+
+        public void PopulateTables()
+        {
+
+        }
     }
 }
