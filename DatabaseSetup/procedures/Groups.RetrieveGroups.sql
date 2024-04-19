02788b6da330ebb55807f2c731d3d6b6d037a268
Joshua Zrubek <joshzru@gmail.com>
2024-04-19 10:19:51 -0500

    Retrive groups now filters based on GroupID or GroupName.

--- DatabaseSetup/procedures/Groups.RetrieveGroups.sql
+++ DatabaseSetup/procedures/Groups.RetrieveGroups.sql
@@ -1,8 +1,12 @@
 CREATE OR ALTER PROCEDURE RetrieveGroups
-
+	@GroupID INT,
+	@GroupName NVARCHAR(50)
 AS 
 
 SELECT G.Name,G.Description
-FROM People.Groups G;
+FROM People.Groups G
+WHERE (@GroupID IS NOT NULL AND @GroupID = G.GroupID)
+	OR (@GroupName IS NOT NULL AND @GroupName = G.GroupName)
+	OR (@GroupID IS NULL AND @GroupName IS NULL);
 
-GO;
+GO
