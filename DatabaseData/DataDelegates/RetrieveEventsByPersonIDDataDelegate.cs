9be62490e49e3f27176b020b8303ede1bee98544
Josh Zrubek <joshzru@gmail.com>
2024-04-20 00:50:19 -0500

    Added RetrieveEventsByPersonIDDataDelegate.

---
+++ DatabaseData/DataDelegates/RetrieveEventsByPersonIDDataDelegate.cs
@@ -0,0 +1,49 @@
+﻿using DataAccess;
+using DataAccess.DataDelegates;
+using DatabaseData.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseData.DataDelegates
+{
+    public class RetrieveEventsByPersonIDDataDelegate : DataReaderDelegate<IReadOnlyList<PersonsEvent>>
+    {
+        private readonly int _personID;
+
+        public RetrieveEventsByPersonIDDataDelegate(int personID)
+            : base("Events.RetrieveEventsByPersonID")
+        {
+            _personID = personID;
+        }
+
+        public override void PrepareCommand(Command command)
+        {
+            base.PrepareCommand(command);
+
+            command.Parameters.AddWithValue("PersonID", _personID);
+        }
+
+        public override IReadOnlyList<PersonsEvent> Translate(Command command, IDataRowReader reader)
+        {
+            List<PersonsEvent> list = new List<PersonsEvent>();
+
+            while (reader.Read())
+            {
+                list.Add(new PersonsEvent(
+                    reader.GetInt32("EventID"),
+                    reader.GetString("Name"),
+                    reader.GetInt32("GroupID"),
+                    reader.GetString("Description"),
+                    reader.GetString("Organizer"),
+                    reader.GetDateTime("Date", DateTimeKind.Utc),
+                    reader.GetInt32("Charge"),
+                    reader.GetBoolean("DidAttend")));
+            }
+
+            return list.AsReadOnly();
+        }
+    }
+}
