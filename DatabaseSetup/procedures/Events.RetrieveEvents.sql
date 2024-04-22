04d50c40b3681fcc10f860d30851470c273012c4
Josh Zrubek <joshzru@gmail.com>
2024-04-21 19:52:53 -0500

    Removed semicolon.

--- DatabaseSetup/procedures/Events.RetrieveEvents.sql
+++ DatabaseSetup/procedures/Events.RetrieveEvents.sql
@@ -5,7 +5,7 @@ AS
 
 SELECT E.EventID, E.Organizer, E.[Name], E.[Date], E.[Description], G.[Name] AS GroupName, E.Charge
 FROM [Events].[Events] E
-    INNER JOIN People.Groups G ON E.GroupID = G.GroupID;
+    INNER JOIN People.Groups G ON E.GroupID = G.GroupID
 WHERE (
         (@EventID IS NOT NULL AND @EventID = E.EventID)
         OR  
