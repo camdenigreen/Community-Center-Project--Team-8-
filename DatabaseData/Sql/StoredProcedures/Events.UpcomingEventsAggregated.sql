03f422957d78e5d520e8a5b976a668e3df899d28
camdenigreen <cigreen@ksu.edu>
2024-04-23 22:04:27 -0500

    Upcoming events query bugfix

--- DatabaseData/Sql/StoredProcedures/Events.UpcomingEventsAggregated.sql
+++ DatabaseData/Sql/StoredProcedures/Events.UpcomingEventsAggregated.sql
@@ -8,7 +8,7 @@ SELECT
     E.EventID,
     E.Name,
     E.Date,
-    SUM(EA.PersonID) AS Registrants
+    COUNT(EA.PersonID) AS Registrants
 FROM Events.Events AS E
 INNER JOIN Events.EventAttendance AS EA ON E.EventID = EA.EventID
 WHERE E.Date >= @CurrentDate
